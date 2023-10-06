004ea7c2787099837c97620f901bed8ff3727467
myusername <myemail>
2023-10-06 16:14:24 +0100

    wrote code for note taking app. have textbox, buttons for adding, deleting, and editing along with a dropdown with a few colour options. need to start javascript for adding note functionality

---
+++ problem-2/scripts.js
@@ -0,0 +1,11 @@
+const notesTextArea = document.getElementById('notes-textarea');
+
+// when addnote clicked, create element in DOM on right
+// side of main page div
+
+// edit note should allow note to be changed
+
+// delete note will remove the last selected element/note
+
+
+notesTextArea.value = ""; // get element textarea and clear input
